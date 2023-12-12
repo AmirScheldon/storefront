fa54e8a52a5aca80c44ab80419afc970953ae536
Amir Scheldon <amirtaslimi12@gmail.com>
2023-12-13 00:38:40 +0330

    modified:   Pipfile 	modified:   Pipfile.lock 	new file:   store/urls.py 	modified:   store/views.py 	modified:   storefront/__pycache__/settings.cpython-311.pyc 	modified:   storefront/__pycache__/urls.cpython-311.pyc 	modified:   storefront/settings.py 	modified:   storefront/urls.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+"""
+    Django:
+        HttpRequest
+        HttpResponse
+    REST Framework:
+        Request
+        Response
+    which is simpler and more powerful than Django's.
+    
+"""
+
+# this func gives use "The browseable api" which make incredibly easy to test our api EndPoint in browser.
+@api_view()
+def product_list(request):
+    return Response('ok')
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
+
